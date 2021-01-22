9461eae31d7a534bb56cb329c74264e7dcdf5775
sakshi12215 <73769344+sakshi12215@users.noreply.github.com>
2021-01-22 21:51:57 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,24 @@
+class Ball{
+    constructor(x,y,width,height,angle){
+        var options={
+            'frictionAir':0.005,
+            'density':0.8
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options)
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body)
+    }
+    display(){
+        var pos=this.body.position
+        var angle = this.body.angle
+        push ()
+        translate(pos.x,pos.y)
+        rotate(angle)
+        strokeWeight(2)
+        stroke("black")
+        fill("red")
+        ellipse(0,0,this.width,this.height)
+        pop()
+    }
+}
